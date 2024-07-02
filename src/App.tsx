3bf83a0e7fdb4fdce7b4a498478e1a012ea357dd
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-07-02 13:08:13 +0200

    Todos section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 export const App = () => {
   return (
-    <div className="bg-[#F5F7F8] h-full p-4 text-[#45474B]">
+    <div className="bg-gradient-to-r from-slate-200 to-slate-300 h-full p-4 text-[#45474B]">
       <div className="max-w-[1200px] m-auto">
         <header>
-          <h1 className="p-2 text-center rounded-md text-white font-semibold text-2xl md:text-4xl mb-8 bg-gradient-to-r from-[#219C90] to-black">
+          <h1 className="p-2 text-center rounded-md font-semibold text-2xl md:text-4xl mb-8">
             Drag & Drop Todo List
           </h1>
         </header>
 
         <main>
-          <div className="input-group flex gap-4">
+          <div className="input-group flex gap-4 mb-12">
             <input
               className="p-2 outline-none w-[80%] text-xl md:text-2xl placeholder:text-neutral-300 rounded-md"
               placeholder="Buy milk"
               id="todo-input"
               type="text"
             />
-            <button className="p-2 w-[20%] rounded-md flex items-center justify-around bg-gradient-to-r from-green-400 to-green-600 hover:bg-pos-100 hover:opacity-90">
+            <button className="p-2 w-[20%] rounded-md flex items-center justify-around bg-green-400 hover:bg-pos-100 hover:opacity-90">
               <p className="text-xl md:text-2xl">Create</p>
             </button>
+          </div>
+
+          <div className="todos-container sm:flex sm:text-center gap-4">
+            <div className="todos mb-8 sm:w-full">
+              <h3 className="text-xl md:text-2xl mb-4 uppercase bg-purple-500 p-2 rounded-md text-white">
+                todo
+              </h3>
+              <div className="min-h-[300px] bg-white rounded-md"></div>
+            </div>
+            <div className="in-progress sm:w-full">
+              <h3 className="text-xl md:text-2xl mb-4 uppercase bg-blue-500 p-2 rounded-md text-white">
+                in progress
+              </h3>
+              <div className="min-h-[300px] bg-white rounded-md"></div>
+            </div>
           </div>
         </main>
       </div>
